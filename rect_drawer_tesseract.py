9b2de74716f5e747a7c1449c0e3e886cad4208c0
Esraa Abdelmaksoud <73304837+esraa-abdelmaksoud@users.noreply.github.com>
2023-04-15 22:49:59 +0200

    Add files via upload

---
+++ rect_drawer_tesseract.py
@@ -0,0 +1,37 @@
+import pytesseract
+from pytesseract import Output
+import cv2
+import os
+import pandas as pd
+import shutil
+# Setting configration
+folder_path ='/mnt/D/Upwork/CompleteMap-Rom/sample_quality_quantification/original_reports/images/'
+config='--psm 3'
+
+
+def draw_boxes(folder_path: str) -> str:
+    boxes_dir = os.path.join(folder_path,"ocr_boxes")
+    if os.path.isdir(boxes_dir):
+        shutil.rmtree(boxes_dir)
+    os.mkdir(boxes_dir)
+    files = os.listdir(folder_path)
+    files.remove("ocr_boxes")
+    for f in range(len(files)):
+        cur_file = os.path.join(folder_path,files[f])
+        img = cv2.imread(cur_file)
+
+        df = pytesseract.image_to_data(img, lang='rus',output_type=Output.DICT,config='{}'.format(config))
+        boxes = len(df['level'])
+
+        for i in range(boxes):
+            if len(df['text'][i]) > 0:
+                (x, y, w, h) = (df['left'][i], df['top'][i], df['width'][i], df['height'][i])
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), thickness=3)
+        output_path = os.path.join(folder_path,"ocr_boxes", files[f])
+        new_df = pd.DataFrame.from_dict(df)
+        new_df.to_csv(f"{output_path[:-4]}.csv")
+        cv2.imwrite(output_path,img)
+    return "Boxes were successfully drawn!"
+
+
+draw_boxes(folder_path)
